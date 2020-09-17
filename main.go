8c1f3a123f04277760b8a5c1e512c875c679775d
diguacheng <467821528@qq.com>
2020-09-17 18:59:08 +0800

    add readme.md file

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 )
 
-//39.97.229.151:9982
+
 func main() {
 	var path = flag.String("filename", "movie_25.csv ", "name of the file to be sent")
 	var address = flag.String("address", "127.0.0.1:9982", "the remote address")
